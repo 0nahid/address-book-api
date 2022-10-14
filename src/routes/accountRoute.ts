36fe3abcb21c6b3cb17f5406047adac0fe9e8034
Naid Hassan Bulbul <nahidhassanbulbul@gmail.com>
2022-10-14 23:43:13 +0600

    jwt token required

--- src/routes/accountRoute.ts
+++ src/routes/accountRoute.ts
@@ -2,14 +2,15 @@ import { Router } from "express";
 const router: Router = Router();
 
 import { accountRouter } from "../controllers/accountController";
+import { verifyToken } from "../middleware/verifyToken";
 
 router.post("/signup", accountRouter.signup);
 router.post("/login", accountRouter.login);
-router.get("/users", accountRouter.getAllUsers);
-router.post("/bulk", accountRouter.addBulkUsers);
-router.get("/user/:id", accountRouter.getUserById);
-router.delete("/user/:id", accountRouter.deleteUser);
-router.delete("/delete/:id", accountRouter.deleteUser);
+router.get("/users", verifyToken, accountRouter.getAllUsers);
+router.post("/bulk",verifyToken, accountRouter.addBulkUsers);
+router.get("/user/:id", verifyToken,accountRouter.getUserById);
+router.delete("/user/:id",verifyToken, accountRouter.deleteUser);
+router.delete("/delete/:id",verifyToken, accountRouter.deleteUser);
 
 
 
